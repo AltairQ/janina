4d9a2f996ace65d3ac7ab0b71af49272970b4737
AltairQ <maciejkamat@gmail.com>
2018-10-20 12:33:47 +0200

    Added err log

--- LOGapi.py
+++ LOGapi.py
@@ -1,9 +1,15 @@
 import datetime 
 
 JS_LOGFILE = open("js.log", "a")
+ERR_LOGFILE = open("err.log", "a")
 
 
 def js_log(text):
 	now = datetime.datetime.now()
 	timestamp = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 	JS_LOGFILE.write("[js " + timestamp + " ] "+text+"\n")
+
+def err_log(text):
+	now = datetime.datetime.now()
+	timestamp = now.strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+	ERR_LOGFILE.write("[err " + timestamp + " ] "+text+"\n")
